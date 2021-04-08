bad1184c3a4794110f3e86a0da5bd2ce7f552922
Gustavo Xavier <gus.guilhermexavier08@gmail.com>
2021-04-08 20:01:03 -0300

    Diagrama de  implementaçao

---
+++ Pratica/Aluno.cs
@@ -0,0 +1,39 @@
+public class Aluno
+{
+	private int id_aluno;
+
+	private string nome;
+
+	private string rg;
+
+	private string cpf;
+
+	private boolean status;
+
+	public boolean IncluirAluno(string nome, string rg, string cpf)
+	{
+		return false;
+	}
+
+	public Aluno ConsultarAluno()
+	{
+		return null;
+	}
+
+	public boolean AlterarAluno(long id_aluno, string nome)
+	{
+		return false;
+	}
+
+	public boolean Excuir aluno(long id_aluno)
+	{
+		return false;
+	}
+
+	public boolean InativarAluno(long id_aluno, boolean status)
+	{
+		return false;
+	}
+
+}
+
---
+++ Pratica/Hospede.cs
@@ -0,0 +1,30 @@
+public class Hospede
+{
+	private long idHospede;
+
+	private string Nome;
+
+	private int Nacionalidade;
+
+	public boolean IncluirHospede(string nome, string nacionalidade)
+	{
+		return false;
+	}
+
+	public boolean AlterarHospede(string Nome, string Nacionalidade, long idHospede)
+	{
+		return false;
+	}
+
+	public Hospede ConsultarHospede(string Nome, string Nacionalidade)
+	{
+		return null;
+	}
+
+	public boolean ExcluirHospede(long idHospede)
+	{
+		return false;
+	}
+
+}
+
